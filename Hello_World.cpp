e33156b3e2d545dacf0e16ae7e696646ffeb791b
LifelesBow737 <diegolm115935@outlook.com>
2022-12-03 13:34:17 -0600

    He creado el repositorio, y agregado un programa basico

---
+++ Hello_World.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    /* code */
+    cout<<"Hola"<<endl;
+    std::cout<<"Hello World"<<std::endl;
+    system("pause");
+    return 0;
+}
