d82ea0108292854b67f0490b6b69ec880f0a9578
LifelesBow737 <diegolm115935@outlook.com>
2022-12-06 12:43:04 -0600

    Borre a jason

--- Hello_World.cpp
+++ Hello_World.cpp
@@ -3,6 +3,8 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     /* code */
+    cout<<"Programa: Hello world"<<endl;
+    cout<<
     cout<<"Hola"<<endl;
     std::cout<<"Hello World"<<std::endl;
     system("pause");
