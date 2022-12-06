78f7d5e70e8da6c1e2be06ff586ca34444ed84d8
LifelesBow737 <118024366+LifelesBow737@users.noreply.github.com>
2022-12-06 16:59:38 -0600

    Branch 2 (#5)
    * Borre a jason
    
    * Agrego un nuevo programa a una branch.

--- Hello_World.cpp
+++ Hello_World.cpp
@@ -3,10 +3,15 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     /* code */
-    cout<<"Este es un tercer cambio jsjsjs (Risa registrada por DIEGO)"<<endl;
+    cout<<"Programa: Hello world"</<endl;
+    cout<<
+    cout<<"Hola"<<endl;
+    std::cout<<"Hello World"<<std::endl;
+   cout<<"Este es un tercer cambio jsjsjs (Risa registrada por DIEGO)"<<endl;
     for(int i=0; i<5; i++){
         cout<<"Hola Mundo"<<endl;
     }
+
     system("pause");
     return 0;
 }
---
+++ Nevada.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hola Diego, como estas?"<<endl;
+    system("pause");
+    return 0;
+}
