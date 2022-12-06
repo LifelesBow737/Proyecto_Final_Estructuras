0b37129c877fbeb5c1d46954b738d4e2ff25a178
AleisterII <118698761+AleisterII@users.noreply.github.com>
2022-12-06 12:03:53 -0600

    cambie el texto

--- Hello_World.cpp
+++ Hello_World.cpp
@@ -4,7 +4,7 @@ int main(int argc, char const *argv[])
 {
     /* code */
     cout<<"Hola"<<endl;
-    std::cout<<"Hello World"<<std::endl;
+    std::cout<<"Hello World this is a new branch"<<std::endl;
     system("pause");
     return 0;
 }
