d3ad0f16389cf66197ae9d12d426e6e6f4760a50
AleisterII <118698761+AleisterII@users.noreply.github.com>
2022-12-06 12:44:33 -0600

    Segundo cambio

--- Hello_World.cpp
+++ Hello_World.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     /* code */
+    cout<<"Este es un segundo cambio, ojala funcione"<<endl;
     for(int i=0; i<5; i++){
         cout<<"Hola Mundo"<<endl;
     }
